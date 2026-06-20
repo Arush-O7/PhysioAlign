1a6150a648f3bc2f2fbfd051cb213ffb0ad049df
Arush-O7 <arushmishra25905@gmail.com>
2026-06-20 16:30:00 +0530

    change logo color to gold

--- src/components/primitives.tsx
+++ src/components/primitives.tsx
@@ -464,7 +464,7 @@ export function PhysioLogo({ size = 80, className = '' }) {
       }}
     >
       <img 
-        src="/logo.png?v=3" 
+        src="/logo.png?v=4" 
         alt="PhysioAlign Logo" 
         style={{ 
           width: '100%', 
